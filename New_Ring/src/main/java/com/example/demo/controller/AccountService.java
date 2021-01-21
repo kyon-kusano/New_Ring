2f50e4aab217667518e4359e96edbe3213f4f2fd
内里竜也 <r-uchizato@y-i-group.co.jp>
2021-01-22 02:37:04 +0900

    login_function
    ログイン機能実装

---
+++ New_Ring/src/main/java/com/example/demo/controller/AccountService.java
@@ -0,0 +1,54 @@
+package com.example.demo.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+@Service
+public class AccountService implements UserDetailsService {
+
+	@Autowired
+	private AccountRepository repository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	private List<Employee> result;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		if (email == null || "".equals(email)) {
+			throw new UsernameNotFoundException("Username is empty");
+		}
+
+		Employee user = repository.findByEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found: " + email);
+		}
+
+		return user;
+	}
+
+	// adminを登録するメソッド
+		@Transactional
+		public void registerAdmin(String username, String password, String email, Date birthday, String sex, String department, String telephone_Number,
+				   String address, Date join_date, Date updated_at, Date deleted_at) {
+			Employee user = new Employee(username, passwordEncoder.encode(password), email, birthday, sex, department, telephone_Number,
+										address, join_date, updated_at, deleted_at);
+			user.setAdmin(true);
+			repository.save(user);
+		}
+
+	public List<Employee> findAllList() {
+		result = repository.findAll();
+		return result;
+	}
+
+
+}
--- New_Ring/src/main/java/com/example/demo/controller/SampleController.java
+++ New_Ring/src/main/java/com/example/demo/controller/SampleController.java
@@ -1,33 +1,50 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class SampleController {
-	
-	@GetMapping("login")
-	private String Login() {
+
+
+	@Autowired
+	private AccountRepository repository;
+
+	public List<Employee> get() {
+		return (List<Employee>) repository.findAll();
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "redirect:/top";
+	}
+
+	@GetMapping("/login")
+	public String login() {
 		return "login";
 	}
-	
-	@GetMapping("index")
-	private String Index() {
-		return "index";
+
+	@PostMapping("/login")
+	public String loginPost() {
+		return "redirect:/login-error";
 	}
-	
-	@GetMapping("detail")
-	private String Detail() {
-		return "detail";
+
+	@GetMapping("/login-error")
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		return "login";
 	}
-	
-	@GetMapping("update")
-	private String Edit() {
-		return "update_form";
+
+	@RequestMapping("/top")
+	public String top() {
+		return "/top";
 	}
-	
-	@GetMapping("create")
-	private String Create() {
-		return "create_form";
-	}
+
 }
