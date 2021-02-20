312d707c2da91e20e8149c889a6c06d19836a85c
soutakagawa <souta8kagawa@gmail.com>
2021-02-20 18:07:16 +0900

    一部修正

--- New_Ring/src/main/resources/static/js/PostcodeJP.js
+++ New_Ring/src/main/resources/static/js/PostcodeJP.js
@@ -1,8 +1,8 @@
 var initPostcodeJp = function() {
 	var postcodeJp = new postcodejp.address.AutoComplementService('OGr8zzCsIjIfr6XZlp7yFZ3Bfw9hRHbgO8x5rF7');
-	postcodeJp.setZipTextbox('input_zip')
-	postcodeJp.add(new postcodejp.address.StateTownTextbox('input_state_town'));
-	postcodeJp.add(new postcodejp.address.StreetTextbox('input_street'));
+	postcodeJp.setZipTextbox('post_Number')
+	postcodeJp.add(new postcodejp.address.StateTownTextbox('address1'));
+	postcodeJp.add(new postcodejp.address.StreetTextbox('address2'));
 	postcodeJp.setClearAddressIfNotFound(true);
 	postcodeJp.observe();
 }
